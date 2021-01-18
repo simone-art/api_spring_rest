696a53a048b14683d6674357c0831a0756c454fc
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-18 10:55:38 -0300

    Criado código para o método buscar cliente por Id

--- src/main/java/com/algaworks/osworks/apicontroller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/apicontroller/ClienteController.java
@@ -5,6 +5,7 @@ import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
@@ -12,6 +13,7 @@ import javax.persistence.PersistenceContext;
 import javax.swing.text.html.parser.Entity;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 
 //@ResController é uma anotacão que permite scannear o projeto con controller
@@ -37,8 +39,16 @@ public class ClienteController {
 
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-//    return clienteRepository.findAll();
+    return clienteRepository.findAll();
 //        return clienteRepository.findByNome("Simone Santos");
-        return clienteRepository.findByNomeContaining("Si");
+//        return clienteRepository.findByNomeContaining("a");
     }
+
+    @GetMapping("/clientes/{clienteId}")
+    public Cliente buscar(@PathVariable Long clienteId){
+        Optional<Cliente> cliente =  clienteRepository.findById(clienteId);
+        return cliente.orElse(null);
+
+    }
+
 }
