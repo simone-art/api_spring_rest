4e19f6b3abf18a168fbb4e6a72080547c312d9fa
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-18 15:42:02 -0300

    Criado código para pegar o campo que não está preenchido e devolver mensagem para o usuário

--- src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,12 +4,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 //@ControllerAdvice componente que trata as exceptions
 
@@ -20,12 +24,21 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid
             (MethodArgumentNotValidException ex, HttpHeaders headers,
              HttpStatus status, WebRequest request) {
+        var campos = new ArrayList<RespostaProblema.Campo>();
+
+        for(ObjectError error:ex.getBindingResult().getAllErrors()) {
+            //String nome = error.getObjectName();
+            String nome = ((FieldError)error).getField();
+            String mensagemCampo = error.getDefaultMessage();
+            campos.add(new RespostaProblema.Campo(nome, mensagemCampo));
+        }
 
         var respostaproblema = new RespostaProblema();
         respostaproblema.setStatus(status.value());
         respostaproblema.setMensagem("Um o mais campos estão inválidos. "
                         + "Faça o preenchimento correto e tente novamente");
         respostaproblema.setDataHora(LocalDateTime.now());
+        respostaproblema.setCampos(campos);
 
         return super.handleExceptionInternal(ex, respostaproblema, headers, status, request);
     }
--- src/main/java/com/algaworks/osworks/api/exceptionhandler/RespostaProblema.java
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/RespostaProblema.java
@@ -2,12 +2,49 @@ package com.algaworks.osworks.api.exceptionhandler;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class RespostaProblema {
 
     private Integer status;
     private LocalDateTime dataHora;
     private String mensagem;
+    private List<Campo> campos;
+
+    public static class Campo{
+        private String nome;
+        private String mensagemCampo;
+
+        //Feito constructoer para pegar o nome e a mensagem do campo
+        public Campo(String nome, String mensagemCampo) {
+            this.nome = nome;
+            this.mensagemCampo = mensagemCampo;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getMensagemCampo() {
+            return mensagemCampo;
+        }
+
+        public void setMensagemCampo(String mensagemCampo) {
+            this.mensagemCampo = mensagemCampo;
+        }
+    }
+
+    public List<Campo> getCampos() {
+        return campos;
+    }
+
+    public void setCampos(List<Campo> campos) {
+        this.campos = campos;
+    }
 
     public Integer getStatus() {
         return status;
