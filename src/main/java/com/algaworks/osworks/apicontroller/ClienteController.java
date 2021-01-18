9016213ac66d713e66d35913c99e39fa233d6a66
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-18 11:36:55 -0300

    Usado o @RequestMapping(/clientes) para retornar tudo o que estiver no /clientes

--- src/main/java/com/algaworks/osworks/apicontroller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/apicontroller/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
@@ -17,6 +18,9 @@ import java.util.Optional;
 
 //@ResController é uma anotacão que permite scannear o projeto con controller
 @RestController
+//@RequestMapping é uma anotação que responde a tudo que estiver no /clientes
+// é aí no GetMapping não precisa colocar o path.
+
 
 //Clase responsável por receber as requisicões externas HTPP e responder
 public class ClienteController {
@@ -36,14 +40,14 @@ public class ClienteController {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar(){
     return clienteRepository.findAll();
 //        return clienteRepository.findByNome("Simone Santos");
 //        return clienteRepository.findByNomeContaining("a");
     }
 
-    @GetMapping("/clientes/{clienteId}")
+    @GetMapping("/{clienteId}")
     public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
         Optional<Cliente> cliente =  clienteRepository.findById(clienteId);
         if (cliente.isPresent()) {
